6fa5efaccc40d553ecff04d7d06877ec7c47b6b2
BABULAL MANDAL <166976966+fullstackbabulal@users.noreply.github.com>
2024-04-14 17:44:48 +0530

    Learn Data Types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553; // Account ID is constant, cannot be changed
+
+let accountEmail = "hitesh@google.com"; // Email can be changed
+
+var accountPassword = "12345"; // Password can also be changed
+
+accountCity = "Jaipur"; // City is declared without any keyword
+
+let accountState; // State variable is declared but not assigned a value
+
+// Changing the values of variables
+accountEmail = "hc@hc.com";
+accountPassword = "21212121";
+accountCity = "Bengaluru";
+
+// Console logging accountId
+console.log(accountId);
+
+// Printing variables in a table format
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // Ensures modern JavaScript standards
+
+// Alert is for browsers, not Node.js
+// alert(3 + 3);
+
+// Printing to console with high readability
+console.log(3 + 3);
+
+console.log("Hitesh"); // Printing a string
+
+let name = "hitesh"; // String
+let age = 18; // Number
+let isLoggedIn = false; // Boolean
+let state; // Undefined
+
+// JavaScript Data Types:
+// Number: 2 to the power of 53, BigInt, String, Boolean, Null, Undefined, Symbol (unique)
+// Object
+
+console.log(typeof undefined); // Outputting the type of undefined
+console.log(typeof null); // Outputting the type of null (Note: It should ideally be "object")
