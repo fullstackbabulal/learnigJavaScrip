0ab75af33150ae3915c352b474ce7a503ac3cfa6
BABULAL MANDAL <166976966+fullstackbabulal@users.noreply.github.com>
2024-04-14 18:39:44 +0530

    variable declaration and type conversion to various operations performed in JavaScript

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,51 @@
+let score = "hitesh"; // Declaring a variable named 'score' and assigning it a string value "hitesh"
+
+// Converting 'score' to a number
+let valueInNumber = Number(score); // Number() converts the string value to a number
+
+// Converting 'score' to a boolean
+let booleanIsLoggedIn = Boolean(score); // Boolean() converts the string value to a boolean
+
+// Converting a number to a string
+let someNumber = 33;
+let stringNumber = String(someNumber); // String() converts the number to a string
+
+let value = 3;
+let negValue = -value; // Negative of 'value'
+
+// Basic arithmetic operations
+console.log(2 + 2); // Addition
+console.log(2 - 2); // Subtraction
+console.log(2 * 2); // Multiplication
+console.log(2 ** 3); // Exponentiation
+console.log(2 / 3); // Division
+console.log(2 % 3); // Modulus (Remainder)
+
+let str1 = "hello";
+let str2 = " hitesh";
+let str3 = str1 + str2; // Concatenation of strings 'str1' and 'str2'
+
+// String and number concatenation
+console.log("1" + 2); // "12"
+console.log(1 + "2"); // "12"
+console.log("1" + 2 + 2); // "122"
+console.log(1 + 2 + "2"); // "32"
+
+// Operator precedence
+console.log((3 + 4) * 5 % 3); // ((3 + 4) * 5) % 3 = 5
+
+// Unary plus operator
+console.log(+true); // 1
+console.log(+ ""); // 0
+
+// Assignment operators
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2; // Assigning the value of '2 + 2' to 'num1', 'num2', and 'num3'
+
+// Incrementing a variable
+let gameCounter = 100;
+++gameCounter; // Incrementing 'gameCounter' by 1
+console.log(gameCounter); // Outputting the incremented value
+
+// Link to study material
+// The link provided is for further reading on type conversion in JavaScript.
